f9edef7d3c0cbbd58e53f8828c1fec778535cc03
Rafey10 <Rafey10@gmail.com>
2019-09-27 02:44:44 +0200

    Update PrimSphere.h

--- src/PrimSphere.h
+++ src/PrimSphere.h
@@ -15,8 +15,8 @@ public:
 	 * @param center Position of the center of the sphere
 	 * @param radius Radius of the sphere
 	 */
-	CPrimSphere(Vec3f center, float radius)
-		: CPrim()
+	CPrimSphere(Vec3f color, Vec3f center, float radius)
+		: CPrim(color)
 		, m_center(center)
 		, m_radius(radius)
 	{}
@@ -24,7 +24,30 @@ public:
 
 	virtual bool Intersect(Ray &ray) override
 	{
-		// --- PUT YOUR CODE HERE ---
+		Vec3f d= ray.org - m_center;
+		
+		float a = ray.dir.dot(ray.dir);
+		float b = 2*ray.dir.dot(d);
+		float c = d.dot(d) - (m_radius*m_radius);
+		
+		float det = (b*b) - 4*a*c;
+		
+		if (det<0){
+			return false;
+		}
+		
+		float t1 = (-b - sqrt(det))/a;
+		
+		if (t1<Epsilon){
+			t1 = (-b + sqrt(det))/a;
+		}
+		
+		if (t1<Epsilon || t1 >ray.t){
+			return false;
+		}
+		
+		ray.t = t1;
+		
 		return true;
 	}
 	
